5cfe1954c1bacae749ff6b4550fd753ff803163f
Fred's GitHub <stopchemtrailsfred@gmail.com>
2024-08-17 11:26:58 +0200

    wip: more UI elements

--- ng-rebrand/src/app/modules/syllogimous/syllogimous.component.ts
+++ ng-rebrand/src/app/modules/syllogimous/syllogimous.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
+import { Engine } from "./engine/engine";
+import { Question } from "./engine/engine-models";
 
-export enum EnumPhases {
+export enum EnumScreens {
     Start = "Start",
     Tutorial = "Tutorial",
     InGame = "In Game",
@@ -14,18 +16,31 @@ export enum EnumPhases {
     styleUrls: ["./syllogimous.component.css"]
 })
 export class SyllogimousComponent {
+    EnumScreens = EnumScreens;
     
-    phaseIndex = 0;
-    phases = [
-        EnumPhases.Start,
-        EnumPhases.Tutorial,
-        EnumPhases.InGame,
-        EnumPhases.Feedback,
-        EnumPhases.LevelUp,
-    ];
+    screen = EnumScreens.Start;
 
-    phaseNext() {
-        this.phaseIndex = (this.phaseIndex + 1) % this.phases.length;
+    engine = new Engine();
+    question?: Question;
+
+    start() {
+        // TODO:
+        // 1. Generate question
+        this.question = this.engine.createSyllogism(3);
+        // 2. Check if to display the tutorial
+        this.screen = EnumScreens.Tutorial;
+        // 3. Go to In Game phase
+    }
+
+    skipTutorial(dontShowAnymore: boolean) {
+        // TODO:
+        // 1. Register into LS the information
+        // 2. Go to In Game phase
+        this.screen = EnumScreens.InGame;
+    }
+
+    checkQuestion(value: boolean) {
+        this.screen = EnumScreens.Feedback;
     }
 
     /*
