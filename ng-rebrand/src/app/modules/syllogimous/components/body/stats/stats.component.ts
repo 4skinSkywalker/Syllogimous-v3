adb5fe10787168a4d193f47f8677e2f1c8fb40ab
Fred's GitHub <stopchemtrailsfred@gmail.com>
2024-09-07 19:27:41 +0200

    feat: complete stats

--- ng-rebrand/src/app/modules/syllogimous/components/body/stats/stats.component.ts
+++ ng-rebrand/src/app/modules/syllogimous/components/body/stats/stats.component.ts
@@ -34,6 +34,9 @@ export class BodyStatsComponent {
     mostCommonMistake = "";
     leastCommonMistake = "";
 
+    types: string[] = [];
+    typeBasedStats: Record<string, { type: string, completed: number, accuracy: number, stats: Record<string, any> }> = {};
+
     constructor(
         public sylSrv: SyllogimousService
     ) {}
@@ -113,6 +116,43 @@ export class BodyStatsComponent {
         const sorted = Object.entries(typeMistakesCount).sort((a, b) => a[1] - b[1]);
         this.mostCommonMistake = sorted[sorted.length - 1][0];
         this.leastCommonMistake = sorted[0][0];
+
+        this.types = [ ...new Set(this.questions.map(q => q.type)) ];
+        this.typeBasedStats = this.types.reduce((acc, curr) => (acc[curr] = { type: "", completed: 0, accuracy: 0, stats: {} }, acc), {} as Record<string, any>);
+        this.types
+            .map(type => this.questions.filter(q => q.type === type))
+            .forEach((questions, idx) => {
+                const type = this.types[idx];
+
+                this.typeBasedStats[type].type = type;
+                this.typeBasedStats[type].completed = questions.length;
+                this.typeBasedStats[type].accuracy = questions.length && (questions.filter(q => q.userAnswer === q.isValid).length / questions.length);
+
+                questions.forEach(q => {
+                    const ps = q.premises.length;
+                    this.typeBasedStats[type].stats[ps] = this.typeBasedStats[type].stats[ps] || {
+                        sum: 0,
+                        count: 0,
+                        fastest: 0,
+                        slowest: 0,
+                    };
+        
+                    const dt = q.answeredAt - q.createdAt;
+                    this.totalPlayTime += dt;
+        
+                    this.typeBasedStats[type].stats[ps].sum += dt;
+                    this.typeBasedStats[type].stats[ps].count += 1;
+        
+                    if (q.userAnswer !== undefined) {
+                        if (this.typeBasedStats[type].stats[ps].fastest === 0 || dt < this.typeBasedStats[type].stats[ps].fastest) {
+                            this.typeBasedStats[type].stats[ps].fastest = dt;
+                        }
+                        if (this.typeBasedStats[type].stats[ps].slowest === 0 || dt > this.typeBasedStats[type].stats[ps].slowest) {
+                            this.typeBasedStats[type].stats[ps].slowest = dt;
+                        }
+                    }
+                });
+            });
     }
 
     formatTime(ms: number): string {
