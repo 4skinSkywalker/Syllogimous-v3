b12980deff660c2c4c1549924e6b00e8fffed0c0
Fred's GitHub <stopchemtrailsfred@gmail.com>
2024-09-07 19:41:28 +0200

    refactor: avoid leaking question types into tutorials screen

--- ng-rebrand/src/app/modules/syllogimous/components/body/tutorial/tutorials/tutorials.component.ts
+++ ng-rebrand/src/app/modules/syllogimous/components/body/tutorial/tutorials/tutorials.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { SyllogimousService } from "../../../../syllogimous.service";
+import { Question } from "src/app/modules/syllogimous/models/question.models";
+import { LS_HISTORY } from "src/app/modules/syllogimous/constants/local-storage.constants";
 
 @Component({
     selector: "app-body-tutorials",
@@ -7,10 +9,18 @@ import { SyllogimousService } from "../../../../syllogimous.service";
     styleUrls: ["./tutorials.component.css"]
 })
 export class BodyTutorialsComponent {
+    questions: Question[] = [];
+    seenQs: Record<string, boolean> = {};
 
     constructor(
         public sylSrv: SyllogimousService
     ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        const history = localStorage.getItem(LS_HISTORY);
+        if (history) {
+            this.questions = JSON.parse(history).reverse();
+            this.seenQs = this.questions.reduce((acc, curr) => (acc[curr.type] = true, acc), {} as any);
+        }
+    }
 }
